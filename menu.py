f9bfcad3725b343b6621239e6bd0b6562b032908
MandaarRao612 <82019376+MandaarRao612@users.noreply.github.com>
2023-02-24 12:02:48 +0530

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,3 +1,84 @@
+import scanner
+import os
+import subprocess
+
+
+
+# Define functions for each menu option
+def webscanpart():
+    print("\n\nWEB APPLICATION VULNERABILITY TESTING")
+    print("*******************************************\n")
+
+    ###### ENTER TARGET URL TO ATTACK ######
+
+    attacksite = input("\n [*] ENTER TARGET URL TO CONTINUE : ")
+
+    ##### PASSING ATTACK URL TO MAIN CODE #####
+
+    webscanner.target = attacksite
+
+    ###### SCAN POLICY ######
+
+    ## FALSE > ALL ATTACKS
+    ## TRUE > SELECTED ATTACKS
+
+    print("\n1. SELECT ATTACKS")
+    print("\n2. PERFORM ALL ATTACKS (takes time)")
+    scanpolicy = input("[*] Enter your choice (1 or 2) : ")
+
+    if scanpolicy == 1:
+        webscanner.useScanPolicy = True
+
+        ###### OPEN ATTACK ID LIST ######
+
+        os.startfile('attacks in zap.xlsx')
+
+        ###### TAKE ATTACK IDs FROM USER ######
+
+        num_list = []
+        n = int(input("\nEnter the number of attacks you want to perform: "))
+        for i in range(n):
+            num = int(input("Enter ID for attack {}: ".format(i + 1)))
+            num_list.append(num)
+        print("\n\nYou have selected the following attack IDs:", num_list)
+
+        webscanner.ascanIds = num_list
+
+    elif scanpolicy == 2:
+        webscanner.useScanPolicy = False
+
+    else:
+        print("\nInvalid Choice")
+
+
+    ###### SET ATTACK STRENGTH ######
+
+    print(" Attack Strength : ")
+    print("\nCan be: 1. Low  |  2. Medium  |  3. High  ")
+    strength = int(input("\n Enter your choice (1,2,3) : "))
+
+    if strength == 1:
+        webscanner.attackStrength = 'Low'
+    elif strength == 2:
+        webscanner.attackStrength = 'Medium'
+    elif strength == 3:
+        webscanner.attackStrength = 'High'
+    else:
+        print("Invalid Choice")
+
+
+subprocess.call(['python', 'webscanner.py'])
+
+
+
+def iotpart():
+    print("\n\nIoT VULNERABILITY TESTING")
+    print("*******************************************\n")
+
+
+
+
+
 print("\n\n")
 print("********************************************************************************\n\n")
 print(" _   _      _      ____    ____    ___   _   _    ____   _____   ____   ")
@@ -7,45 +88,22 @@ print("|  _  |  / ___ \  |  _ <  | |_) |  | |  | |\  | | |_| | | |___  |  _ <  "
 print("|_| |_| /_/   \_\ |_| \_\ |____/  |___| |_| \_|  \____| |_____| |_| \_\ ")
 print("\n\n********************************************************************************\n\n")
 
-
-# Define functions for each menu option
-def option1():
-    print("\n\nWEB APPLICATION VULNERABILITY TESTING")
-    print("*******************************************\n")
-    attacksite = input("\n [*] ENTER TARGET URL TO CONTINUE : ")
-
-
-    while True:
-        print("\n")
-        print("\n 1. SELECT ATTACKS")
-        print("\n 2. ALL ATTACKS ")
-
-        attackchoice = input("\n\n ENTER YOUR CHOICE: ")
-
-        
-
-
-
-def option2():
-    print("\n\nIoT VULNERABILITY TESTING")
-    print("*******************************************\n")
-
-
 while True:
     print("Menu:")
     print("1. Web Application Vulnerability Testing")
     print("2. Iot Vulnerability Testing")
     print("3. Exit")
 
-    choice = input("Enter your choice: ")
+    choice = input("[*] Enter your choice: ")
 
     # Call the appropriate function based on the user's choice
     if choice == "1":
-        option1()
+        webscanpart()
     elif choice == "2":
-        option2()
+        iotpart()
     elif choice == "3":
         break
     else:
-        print("Invalid choice. Please enter a number between 1 and 5.")
+        print("Invalid choice. Please enter a number between 1 and 3.")
+
 
